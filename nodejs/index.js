d522af4a2498c391aa5f4165284c161605c5ced9
Petri Asunmaa <petrieas@gmail.com>
2019-02-21 10:33:48 +0200

    Added date and time

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,3 +1,7 @@
 module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 2.0!`);
+  res.end(`Hello from Node.js on Now 2.0!` + new Date());
 };
+
+/* module.exports = (req, res) => {
+  res.end('The time is: ' + new Date())
+} */
